f63a409faf437329a242fe7538bba9a61f552f3a
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-06-13 11:53:01 +0200

    Fixed the build

--- src/html.js
+++ src/html.js
@@ -1,40 +1,45 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class HTML extends React.Component {
-  render() {
-    return (
-      <html {...this.props.htmlAttributes}>
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <meta name="monetization" content="$ilp.uphold.com/NBQEYEy4h3kA">
-          {this.props.headComponents}
-        </head>
-        <body {...this.props.bodyAttributes}>
-          {this.props.preBodyComponents}
-          <noscript>This site runs best with JavaScript enabled.</noscript>
-          <div
-            key={`body`}
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
-          />
-          {this.props.postBodyComponents}
-        </body>
-      </html>
-    )
-  }
+    render() {
+        return (
+            <html {...this.props.htmlAttributes}>
+                <head>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    <meta
+                        name="monetization"
+                        content="$ilp.uphold.com/NBQEYEy4h3kA"
+                    />
+                    {this.props.headComponents}
+                </head>
+                <body {...this.props.bodyAttributes}>
+                    {this.props.preBodyComponents}
+                    <noscript>
+                        This site runs best with JavaScript enabled.
+                    </noscript>
+                    <div
+                        key={`body`}
+                        id="___gatsby"
+                        dangerouslySetInnerHTML={{ __html: this.props.body }}
+                    />
+                    {this.props.postBodyComponents}
+                </body>
+            </html>
+        );
+    }
 }
 
 HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
-}
+    htmlAttributes: PropTypes.object,
+    headComponents: PropTypes.array,
+    bodyAttributes: PropTypes.object,
+    preBodyComponents: PropTypes.array,
+    body: PropTypes.string,
+    postBodyComponents: PropTypes.array,
+};
